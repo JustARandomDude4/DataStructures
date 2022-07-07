f226bf7c55d1fcbedfa9d1cfde5cf389194bc595
Hima Satwika Katragadda <83648082+JustARandomDude4@users.noreply.github.com>
2022-07-07 15:32:49 +0530

    Update Quick_Sort.c

---
+++ DivideAndConquer/Quick_Sort.c
@@ -0,0 +1,61 @@
+#include <stdio.h>
+void swap(int * a, int* b)
+{
+    int  temp=*a;
+    *a=*b;
+    *b=temp;
+}
+int partition(int *arr,int left,int right)
+{
+    int i=left;   
+    int j=right;  
+    int pivot=arr[left];
+    
+    do
+    {
+        do
+        {
+            i++;                   // increments until ele > pivot is found
+        }while(arr[i]<=pivot);
+        
+        do
+        {
+            j--;                  // decrements until ele < pivot is found
+        }while(arr[j]>pivot);
+        
+        swap(&arr[i],&arr[j]);   // once i, j are found, elements swapped to proper places
+        
+    }while(i<j);
+    
+    int pivotIndex=j;
+    swap(&arr[left],&arr[pivotIndex]);  // once sorting around pivot done, pivot is swapped to its correct place
+
+    return pivotIndex;
+}
+void QuickSort(int arr[],int left,int right)
+{
+
+    int pivotIndex;
+    if (left<right)
+    {
+        pivotIndex=partition(arr,left,right);
+        QuickSort(arr,left,pivotIndex);
+        QuickSort(arr,pivotIndex+1,right);
+    }
+}
+int main()
+{    
+    int arr[]={5,2,1,7,17,20,3};
+    
+    printf("Before sorting : \n");
+    for(int i=0;i<7;i++)
+    printf(" %d ",arr[i]);
+    
+    QuickSort(arr,0,6);
+        
+    printf("\nAfter sorting : \n");
+    for(int i=0;i<7;i++)
+    printf(" %d ",arr[i]);
+
+  return 0;  
+}
