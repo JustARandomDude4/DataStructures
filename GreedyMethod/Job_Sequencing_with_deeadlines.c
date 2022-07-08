b5a8aabc341061dde4e0b0ca0748eaa4247ffc65
Hima Satwika Katragadda <83648082+JustARandomDude4@users.noreply.github.com>
2022-07-08 15:48:37 +0530

    Update Job_Sequencing_with_deeadlines.c

---
+++ GreedyMethod/Job_Sequencing_with_deeadlines.c
@@ -0,0 +1,70 @@
+#include <stdio.h>
+
+//input : total jobs, profits in decreasing order , deadlines
+//output : sequence of jobs
+
+int maxDeadline(int *a,int n)
+{
+    int max=a[0];
+    for(int i=1;i<n;i++)
+    if(a[i]>max)
+     max=a[i];
+         printf("%d",max);
+    return max;
+}
+int AllotSlot(int * slots,int deadLine, int JobNo)
+{
+    for(int i=deadLine-1;i>=0;i--)
+    {
+            if (slots[i]==-1)
+               {
+                   slots[i]=JobNo;
+                   printf(" *%d %d* ",i,slots[i]);
+                   return 1;
+                }
+    }
+
+return -1;
+}
+
+int JobSequencingWithDeadLines(int n, int *d, int *p)
+{  
+    int m=maxDeadline(d,n);  //3
+    int slots[m];
+    for(int i=0;i<m;i++)   // initially, no slot is allocated to any slot so initialise to -1
+    slots[i]=-1;
+    
+    for(int i=0;i<n;i++)
+    {
+        printf(" /%d/ ",i);
+
+        AllotSlot(slots,d[i],i+1);
+    }
+    
+    for(int i=0;i<m;i++)
+    if(slots[i]!=-1)
+    printf(" J%d ",slots[i]);
+    
+    
+}
+int main()
+{
+    int n, kC;
+    printf("Enter the total no of jobs\n");
+    scanf("%d",&n);
+    
+    
+    int d[n],p[n];
+    printf("Enter the deadlines of jobs :\n");
+    for(int i=0;i<n;i++)
+    scanf("%d",&d[i]);
+    printf("Enter the profits of jobs:\n");
+    for(int i=0;i<n;i++)
+    scanf("%d",&p[i]);
+    
+    
+    printf("The sequence of jobs that is obtained is : ");
+    JobSequencingWithDeadLines(n,d,p);
+    
+    return 0;
+}
