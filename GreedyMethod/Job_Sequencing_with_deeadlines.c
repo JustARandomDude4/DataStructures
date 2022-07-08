4fe738f214f5beabc33ba41274a49494c8ebe0c5
Hima Satwika Katragadda <83648082+JustARandomDude4@users.noreply.github.com>
2022-07-08 16:50:52 +0530

    Update Job_Sequencing_with_deeadlines.c

--- GreedyMethod/Job_Sequencing_with_deeadlines.c
+++ GreedyMethod/Job_Sequencing_with_deeadlines.c
@@ -3,7 +3,7 @@
 //input : total jobs, profits in decreasing order , deadlines
 //output : sequence of jobs
 
-int maxDeadline(int *a,int n)
+int maxDeadline(int *a,int n)  // Finds the highest deadline available in the input to allot the slots
 {
     int max=a[0];
     for(int i=1;i<n;i++)
@@ -12,36 +12,32 @@ int maxDeadline(int *a,int n)
          printf("%d",max);
     return max;
 }
-int AllotSlot(int * slots,int deadLine, int JobNo)
+int AllotSlot(int * slots,int deadLine, int JobNo)   // checks whether a free slot is available or not, if yes, then allots it tot the current Job
 {
-    for(int i=deadLine-1;i>=0;i--)
+    for(int i=deadLine-1;i>=0;i--)  // checks from deadline slot to previous slots if they r empty
     {
-            if (slots[i]==-1)
+            if (slots[i]==-1)  
                {
-                   slots[i]=JobNo;
-                   printf(" *%d %d* ",i,slots[i]);
+                   slots[i]=JobNo; // allocating  the found free slot to the given Jobfound 
                    return 1;
                 }
     }
-
 return -1;
 }
 
-int JobSequencingWithDeadLines(int n, int *d, int *p)
+int JobSequencingWithDeadLines(int n, int *d, int *p)  
 {  
-    int m=maxDeadline(d,n);  //3
+    int m=maxDeadline(d,n);  // finds the no of slots available
     int slots[m];
     for(int i=0;i<m;i++)   // initially, no slot is allocated to any slot so initialise to -1
     slots[i]=-1;
     
-    for(int i=0;i<n;i++)
+    for(int i=0;i<n;i++)   // allocate slots to all the jobs
     {
-        printf(" /%d/ ",i);
-
-        AllotSlot(slots,d[i],i+1);
+        AllotSlot(slots,d[i],i+1);   // if slot is available, allocates else, returns -1
     }
     
-    for(int i=0;i<m;i++)
+    for(int i=0;i<m;i++)  // printing the job sequence
     if(slots[i]!=-1)
     printf(" J%d ",slots[i]);
     
